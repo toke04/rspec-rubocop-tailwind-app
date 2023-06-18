22e47ef0db8a5a605abb658ae6d56513083a8f8d
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-06-18 09:33:12 +0900

    RSpec + factorybot + テストの起動を早くする初期設定を完了

--- Gemfile
+++ Gemfile
@@ -54,6 +54,8 @@ gem 'bootsnap', require: false
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem 'debug', platforms: %i[mri mingw x64_mingw]
+  gem 'rspec-rails'
+  gem 'factory_bot_rails'
 end
 
 group :development do
@@ -63,6 +65,7 @@ group :development do
   gem 'rubocop-rails', require: false # 追加
   gem 'rubocop-rspec' # 追加
   gem 'web-console'
+  gem 'spring-commands-rspec'
 
   # Add speed badges [https://github.com/MiniProfiler/rack-mini-profiler]
   # gem "rack-mini-profiler"
--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,12 @@ module RspecRubocopTailwindApp
 
     config.generators do |g| # ここから追記
       g.assets false          # CSS, JavaScriptファイルを自動生成しない
-      g.helper     false      # helperファイルを自動生成しない
+      g.helper     false
+      g.test_framework :rspec, # ここから5行を追記
+                       fixtures: false, # テストDBにレコードを作るfixtureの作成をスキップ(FactoryBotを使用するため)
+                       view_specs: false, # ビューファイル用のスペックを作成しない
+                       helper_specs: false, # ヘルパーファイル用のスペックを作成しない
+                       routing_specs: false # routes.rb用のスペックファイル作成しない# helperファイルを自動生成しない
     end  # ここまで追記
   end
 end
