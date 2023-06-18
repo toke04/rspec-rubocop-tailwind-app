5bca8c82d2d5708f4e8273613a656e31b2e690c6
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-06-18 10:30:42 +0900

    トップページの統合テストを追加

---
+++ spec/system/homes_spec.rb
@@ -0,0 +1,16 @@
+
+require 'rails_helper'
+
+RSpec.describe 'Home', type: :system do
+  before do
+    driven_by(:rack_test)
+  end
+
+  describe 'トップページの検証' do
+    it 'Home#top という文字列が表示される' do
+      visit '/'
+
+      expect(page).to have_content('Home#top')
+    end
+  end
+end
