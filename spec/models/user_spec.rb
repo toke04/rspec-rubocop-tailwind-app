8d4774f305b6cf9471e3ff8a3c2cec1484880db5
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-06-20 15:14:42 +0900

    deviceのビューファイルの初期バージョン + userモデルの初期テスト

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,48 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User do
+  let(:nickname) { 'テスト太郎' }
+  let(:email) { 'test@example.com' }
+  let(:password) { '12345678' }
+  let(:user) { User.new(nickname: nickname, email: email, password: password, password_confirmation: password) }
+
+  describe '.first' do
+    before do
+      create(:user, nickname: nickname, email: email)
+    end
+
+    subject { described_class.first }
+
+    it '事前に作成した通りのUserを返す' do
+      expect(subject.nickname).to eq('テスト太郎')
+      expect(subject.email).to eq('test@example.com')
+    end
+  end
+
+  describe 'validation' do
+
+    describe 'nickname属性' do
+
+      describe '文字数制限の検証' do
+        context 'nicknameが20文字以下の場合' do
+          let(:nickname) { 'あいうえおかきくけこさしすせそたちつてと' } # 20文字
+
+          it 'User オブジェクトは有効である' do
+            expect(user.valid?).to be(true)
+          end
+        end
+
+        context 'nicknameが20文字を超える場合' do
+          let(:nickname) { 'あいうえおかきくけこさしすせそたちつてとな' } # 21文字
+
+          it 'User オブジェクトは無効である' do
+            # user.valid?
+
+            expect(user.valid?).to be(false)
+            expect(user.errors[:nickname]).to include('is too long (maximum is 20 characters)')
+          end
+        end
+      end
+    end
+  end
 end
